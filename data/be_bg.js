5ca1ab212d7808756bb64f2cc03af2f7273631ee
Alexey Makarov <alexey@norlin.ru>
2014-12-05 19:04:41 +0200

    add icons & testcase result display

--- data/be_bg.js
+++ data/be_bg.js
@@ -2,6 +2,7 @@
     window.apply_test = function(){
         var args = [].slice.call(arguments);
         console.log('arguments received:', args);
+        return args.length;
     };
 
     console.log('bg started', window.ff_test);
